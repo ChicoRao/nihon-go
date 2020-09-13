9ae65d7e759a1be8cf5b62ce2964f6b8b6f3472b
Rico <rchao@sfu.ca>
2020-09-12 17:30:18 -0700

    Updates

---
+++ precache-manifest.379bb8d78081e0331ba697743f918312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790c7d4ba5bb9981fe6de7993c797d85",
+    "url": "/nihon-go/index.html"
+  },
+  {
+    "revision": "41b82ac854cc613a0ff8",
+    "url": "/nihon-go/static/css/main.f5502c2f.chunk.css"
+  },
+  {
+    "revision": "7247d35b87f3dc7fdd9e",
+    "url": "/nihon-go/static/js/2.762d9248.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nihon-go/static/js/2.762d9248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b82ac854cc613a0ff8",
+    "url": "/nihon-go/static/js/main.2de39c8c.chunk.js"
+  },
+  {
+    "revision": "cfb7d3bca182c60a8958",
+    "url": "/nihon-go/static/js/runtime-main.3af7e206.js"
+  }
+]);
